89ed0b2341da95fd70e62c7e9c8345ef3f078deb
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-03-25 00:06:06 -0600

    navbar with navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
       <nav className='navbar navbar-expand-md bg-body-tertiary'>
         <div className='container-fluid'>
-          <a className='navbar-brand' href='#'>Logo</a>
+          <NavLink className='navbar-brand' to='#'>Logo</NavLink>
           <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon' />
           </button>
           <div className='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
               <li className='nav-item'>
-                <a className='nav-link active' aria-current='page' href='#'>Home</a>
+                <NavLink className='nav-link active' aria-current='page' to='/'>Home</NavLink>
               </li>
               <li className='nav-item'>
-                <a className='nav-link' href='#'>Link</a>
+                <NavLink className='nav-link' to='/shows/1'>Shows</NavLink>
               </li>
               {/* <li className='nav-item dropdown'>
-                <a className='nav-link dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+                <NavLink className='nav-link dropdown-toggle' to='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
                   Dropdown
-                </a>
+                </NavLink>
                 <ul className='dropdown-menu'>
-                  <li><a className='dropdown-item' href='#'>Action</a></li>
-                  <li><a className='dropdown-item' href='#'>Another action</a></li>
+                  <li><NavLink className='dropdown-item' to='#'>Action</NavLink></li>
+                  <li><NavLink className='dropdown-item' to='#'>Another action</NavLink></li>
                   <li><hr className='dropdown-divider' /></li>
-                  <li><a className='dropdown-item' href='#'>Something else here</a></li>
+                  <li><NavLink className='dropdown-item' to='#'>Something else here</NavLink></li>
                 </ul>
               </li> */}
             </ul>
