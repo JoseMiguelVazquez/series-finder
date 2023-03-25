f532ba4e5e492c3290846a401d5121ad75a5073d
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-03-25 00:44:37 -0600

    shows details with useParams

--- src/pages/ShowDetails.jsx
+++ src/pages/ShowDetails.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import ImageNotAvailable from '../assets/Image-Not-Available.png'
 
 const ShowDetails = () => {
+  const [show, setShow] = useState(null)
+  // const id = useParams().id
+  const { id } = useParams()
+
+  useEffect(() => {
+    fetch(`https://api.tvmaze.com/shows/${id}`)
+      .then(response => response.json())
+      .then(data => {
+        setShow(data)
+      }).catch(error => {
+        console.log(error)
+      })
+  }, [])
   return (
     <div>
-      Show Details
+      <h1>{show?.name}</h1>
+      <div>
+        <div>
+          <img src={show?.image ? show?.image.medium : ImageNotAvailable} alt='' />
+        </div>
+        {show?.summary}
+      </div>
     </div>
   )
 }
