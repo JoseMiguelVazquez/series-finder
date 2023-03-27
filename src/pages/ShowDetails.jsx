391943fddf943491da16d9e128028bc2deac7883
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-03-27 10:49:06 -0600

    added more show info, episodes and cast

--- src/pages/ShowDetails.jsx
+++ src/pages/ShowDetails.jsx
@@ -4,6 +4,8 @@ import ImageNotAvailable from '../assets/Image-Not-Available.png'
 
 const ShowDetails = () => {
   const [show, setShow] = useState(null)
+  const [episodes, setEpisodes] = useState([])
+  const [cast, setCast] = useState([])
   // const id = useParams().id
   const { id } = useParams()
 
@@ -16,6 +18,27 @@ const ShowDetails = () => {
         console.log(error)
       })
   }, [])
+
+  useEffect(() => {
+    fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
+      .then(response => response.json())
+      .then(data => {
+        setEpisodes(data)
+      }).catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  useEffect(() => {
+    fetch(`https://api.tvmaze.com/shows/${id}/cast`)
+      .then(response => response.json())
+      .then(data => {
+        setCast(data)
+      }).catch(error => {
+        console.log(error)
+      })
+  }, [])
+
   return (
     <>
       <div className='container-md'>
@@ -35,16 +58,63 @@ const ShowDetails = () => {
               <p>
                 {show?.network
                   ? (<><b>Network: </b>{show?.network.name}</>)
-                  : (<><b>Web Channel: </b>{show?.webChannel.name}</>)}
+                  : show?.webChannel
+                    ? (<><b>Web Channel: </b>{show?.webChannel.name}</>)
+                    : (<></>)}
               </p>
+              <p>
+                {(show?.schedule.days === undefined || show?.schedule.days.length === 0)
+                  ? (<></>)
+                  : (<><b>Schedule: </b>{show?.schedule.days + 's'}</>)}
+                {show?.schedule.time !== ''
+                  ? (<> at {show?.schedule.time}</>)
+                  : (<></>)}
+              </p>
+              <p><b>Status: </b>{show?.status}</p>
+              <p>{show?.genres === undefined || show?.genres.length === 0
+                ? (<></>)
+                : (<><b>Genres: </b>
+                  {show?.genres.length === 1
+                    ? show?.genres[0]
+                    : show?.genres.map((element, i, array) => (
+                      i < array.length - 1 ? `${element} | ` : `${element}`
+                    ))}
+                </>)}
+              </p>
+              <p><b>Episodes: </b><a href='#episodes-section'>{episodes.length}</a></p>
             </div>
           </div>
         </div>
-        <div>
-          Episodes
+        <div id='episodes-section'>
+          <h2>Episodes</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Episode Name</th>
+                <th>Airdate</th>
+                <th>Rating</th>
+              </tr>
+            </thead>
+            <tbody>
+              {episodes.map(episode => (
+                <tr key={episode.id}>
+                  <td>{episode.name}</td>
+                  <td>{episode.airdate}</td>
+                  <td>{episode.rating.average}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
         <div>
-          Actors
+          <h2>Cast</h2>
+          <div className='row'>
+            {cast.map(member => (
+              <div key={member.person.id}>
+                <h4>{member.person.name}</h4>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
